634baf9baf39705976294428d1b5e42fee9d7b5c
astankovic <astankovic@users.noreply.github.com>
2018-06-16 17:36:27 +0200

    handling of wrong url

--- get_data.py
+++ get_data.py
@@ -74,7 +74,7 @@ browser.get(url)
 
 # wait for js to load
 print('Waiting for JS to load: ')
-for i in range(6, 1, -1):
+for i in range(10, 1, -1):
     time.sleep(1)
     print(i)
 
@@ -85,96 +85,109 @@ while True:
     html = browser.page_source
     soup = BeautifulSoup(html, 'lxml')
 
-    # get current games
-    live_matches = soup.find_all('div', {'class': 'match live'})
-    live_matches_ids = [x.find(class_='code').string for x in live_matches]
-    print(str(len(live_matches_ids)) + ' matches currently.')
 
-    # check to dump the finished games
-    if games_pool:
-        delete = []
-        if len(live_matches_ids) != 0:
-            for i in games_pool.keys():
-                if i not in live_matches_ids:
-                    print('Game ' + i
-                          + ':' + games_pool[i]['home']
-                          + ' - ' + games_pool[i]['away']
-                          + ' has just finished.')
-                    log.append('Game ' + i
-                          + ':' + games_pool[i]['home']
-                          + ' - ' + games_pool[i]['away']
-                          + ' has just finished.')
-                    log.append('live matches: ' + str(live_matches_ids))
-                    log.append('games pool: ' + str(games_pool.keys()))
-                    try:
-                        write_to_csv(games_pool[i], path_dir)
-                    except Exception as ex:
-                        print('Error writing to file: ' + i + ": " + str(ex))
-                    delete.append(i)
-            if delete:
-                for i in delete:
-                    del games_pool[i]
+    if browser.current_url != url:
+        #proveri da li postoji neki fudbal pa ako da refreshuj i vrati na pocetni url
+        if 'Fudbal' in [i.text for i in soup.find_all('h2')]:
+            browser.get(url)
+            continue
         else:
-            print('Something wrong. Refreshing the page...')
-            log.append('Something wrong. Refreshing the page...')
-            browser.refresh()
+            time.sleep(60)
+            log.append('Currently there are no matches. Passing 30s...')
+            continue
+    else:
+        # get current games
+        live_matches = soup.find_all('div', {'class': 'match live'})
+        live_matches_ids = [x.find(class_='code').string for x in live_matches]
+        print(str(len(live_matches_ids)) + ' matches currently.')
 
-    # get the matches data
-    for i in live_matches:
-        try:
-            game_time = i.find_all(class_='time')[0].string.strip()
-            game_time = int(str.replace(game_time, "'", ""))
-        except:
-            game_time = None
-        if game_time:
-            code = i.find_all(class_='code')[0].string.strip()
-            league = i.find_all(class_='league')[0].string.strip()
+        # check to dump the finished games
+        if games_pool:
+            delete = []
 
-            home = i.find_all(class_='home')[0].string.strip()
-            away = i.find_all(class_='away')[0].string.strip()
-            try:
-                home_result = i.find_all(class_='home')[1].string.strip()
-            except:
-                home_result = None
-            try:
-                away_result = i.find_all(class_='away')[1].string.strip()
-            except:
-                away_result = None
-
-            available_bets = i.find_all('div', class_='game')
-            odds_dict = {}
-            if available_bets:
-                for game in available_bets:
-                    game_type_div = game.div
-                    if game_type_div.string:
-                        game_type = game_type_div.string.strip()
-                        new_dict = {}
-                        for desc in game.descendants:
-                            try:
-                                if desc.get('class')[0] == 'selection-name':
-                                    odd_name = desc.string
-                                    odd = desc.next_sibling.next_sibling.string.strip()
-                                    new_dict[odd_name] = odd
-                            except:
-                                pass
-                        odds_dict[game_type] = new_dict
-
-            translated_odds = translate_odds(mapping, odds_dict)
-            moment = game_time
-            data = {'res_home': home_result,
-                    'res_away': away_result
-                    }
-            data.update(translated_odds)
-
-            if code not in games_pool.keys():
-                game_data = {'code': code,
-                             'league': league,
-                             'home': home,
-                             'away': away,
-                             'history': {moment: data}
-                             }
-                games_pool[code] = game_data
+            #check if there are matches
+            if len(live_matches_ids) != 0:
+                for i in games_pool.keys():
+                    if i not in live_matches_ids:
+                        print('Game ' + i
+                              + ':' + games_pool[i]['home']
+                              + ' - ' + games_pool[i]['away']
+                              + ' has just finished.')
+                        log.append('Game ' + i
+                              + ':' + games_pool[i]['home']
+                              + ' - ' + games_pool[i]['away']
+                              + ' has just finished.')
+                        log.append('live matches: ' + str(live_matches_ids))
+                        log.append('games pool: ' + str(games_pool.keys()))
+                        try:
+                            write_to_csv(games_pool[i], path_dir)
+                        except Exception as ex:
+                            print('Error writing to file: ' + i + ": " + str(ex))
+                        delete.append(i)
+                if delete:
+                    for i in delete:
+                        del games_pool[i]
             else:
-                games_pool[code]['history'][moment] = data
-    time.sleep(refresh_period)
+                print('Something wrong. Refreshing the page...')
+                log.append('Something wrong. Refreshing the page...')
+                browser.refresh()
+
+        # get the matches data
+        for i in live_matches:
+            try:
+                game_time = i.find_all(class_='time')[0].string.strip()
+                game_time = int(str.replace(game_time, "'", ""))
+            except:
+                game_time = None
+            if game_time:
+                code = i.find_all(class_='code')[0].string.strip()
+                league = i.find_all(class_='league')[0].string.strip()
+
+                home = i.find_all(class_='home')[0].string.strip()
+                away = i.find_all(class_='away')[0].string.strip()
+                try:
+                    home_result = i.find_all(class_='home')[1].string.strip()
+                except:
+                    home_result = None
+                try:
+                    away_result = i.find_all(class_='away')[1].string.strip()
+                except:
+                    away_result = None
+
+                available_bets = i.find_all('div', class_='game')
+                odds_dict = {}
+                if available_bets:
+                    for game in available_bets:
+                        game_type_div = game.div
+                        if game_type_div.string:
+                            game_type = game_type_div.string.strip()
+                            new_dict = {}
+                            for desc in game.descendants:
+                                try:
+                                    if desc.get('class')[0] == 'selection-name':
+                                        odd_name = desc.string
+                                        odd = desc.next_sibling.next_sibling.string.strip()
+                                        new_dict[odd_name] = odd
+                                except:
+                                    pass
+                            odds_dict[game_type] = new_dict
+
+                translated_odds = translate_odds(mapping, odds_dict)
+                moment = game_time
+                data = {'res_home': home_result,
+                        'res_away': away_result
+                        }
+                data.update(translated_odds)
+
+                if code not in games_pool.keys():
+                    game_data = {'code': code,
+                                 'league': league,
+                                 'home': home,
+                                 'away': away,
+                                 'history': {moment: data}
+                                 }
+                    games_pool[code] = game_data
+                else:
+                    games_pool[code]['history'][moment] = data
+        time.sleep(refresh_period)
 
