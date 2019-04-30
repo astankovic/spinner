7d7ec69806c422a7d3ce362a191b16a234285f2e
Andrija Stankovic <andrija.stankovic@outlook.com>
2019-04-30 23:02:57 +0200

    webserver refactor - minor change

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import get_data
 if __name__ == '__main__':
     # instantiate pools
     meridian_pool = get_data.GamesPool()
-    meridian_pool.start()
 
     # start web server
     webServer = webserver.WebApp(meridian_pool.pool)
     webServer.app.run(debug=False, port=8181)
+
+    meridian_pool.start()
