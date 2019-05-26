4d7b4e9785f80d9dce3ac56c11131bce0176521b
Andrija Stankovic <andrija.stankovic@outlook.com>
2019-05-26 18:55:31 +0200

    fixed threading

--- app.py
+++ app.py
@@ -6,11 +6,8 @@ if __name__ == '__main__':
     # instantiate pools
     meridian_pool = get_data.GamesPool()
     t1 = Thread(target=meridian_pool.start)
-    t1.setDaemon(True)
-    t1.start()
     # start web server
     webServer = webserver.WebApp(meridian_pool.pool)
     t2 = Thread(target=webServer.app.run, kwargs={'debug': False, 'port': 8181})
-    #webServer.app.run(debug=False, port=8181)
-    t2.setDaemon(True)
     t2.start()
+    t1.start()
